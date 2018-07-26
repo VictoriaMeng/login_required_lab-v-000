50004e6d0dcad992a9f9007ba41cc454934b3c98
Victoria Meng <nights1ststar@protonmail.com>
2018-07-26 15:57:10 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,6 +1,6 @@
 class SecretsController < ApplicationController
   before_action :require_login
-  skip_before_action :require_login, only [:index]
+  skip_before_action :require_login, only: [:index]
 
   def index
     binding.pry
