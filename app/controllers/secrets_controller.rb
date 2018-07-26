08dddc72cf8eecfda31fd8a3fa2ace0c9df22af3
Victoria Meng <nights1ststar@protonmail.com>
2018-07-26 15:20:16 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -8,6 +8,6 @@ class SecretsController < ApplicationController
   private
 
   def require_login
-    return head(:forbidden) unless session.include?(:name)
+    redirect_to unless session.include?(:name)
   end
 end
