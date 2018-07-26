ba9c7db028141c1e1ada2f5932b4bc8cca98b81f
Victoria Meng <nights1ststar@protonmail.com>
2018-07-26 16:12:08 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :secrets, only: [:welcome, :login, :show]
   root 'secrets#login'
-  get '/welcome' => 'secrets#welcome'
-  post '/login' => 'secrets#login'
+  # get '/welcome' => 'secrets#welcome'
+  # post '/login' => 'secrets#login'
 end
