ff9d50a7ffc6bc1d99a4cea2e4f6a33cd15fcd1b
Victoria Meng <nights1ststar@protonmail.com>
2018-07-26 15:47:42 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,18 +1,3 @@
 class SecretsController < ApplicationController
-  before_action :require_login
-  skip_before_action :require_login, only: [:index]
 
-  def show
-
-  end
-
-  def login
-    binding.pry
-  end
-
-  private
-
-  def require_login
-    redirect_to secrets_path unless session.include?(:name)
-  end
 end
